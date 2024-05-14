01bb922dc89ae6d9a40793189ab5578415666cec
Michael Carey <michael@AGameEmpowerment.com>
2024-05-14 23:51:11 +0000

    chore: Add .devcontainer configuration for development environment setup

---
+++ src/PostCreate.ps1
@@ -0,0 +1,14 @@
+# Setup Development Environment DevContainer
+Write-Host "Post Create Commands for Environment..."
+
+cd Api/Api.Startup.Example
+dotnet restore Startup.Api.csproj
+cd ../../Blazor/Blazor.Server.Startup.Example
+dotnet restore Startup.Blazor.Server.csproj
+cd ../../Console/Console.Startup.Example
+dotnet restore Startup.Console.csproj
+cd ../../Web/Web.Startup.Example
+dotnet restore Startup.Web.csproj
+cd ../../
+
+
