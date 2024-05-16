db60edcd7400b3618c7e29f969a9246ce750b7dd
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-16 19:15:03 +0000

    chore: Remove unused Bootstrap SCSS files

--- src/PostCreate.ps1
+++ src/PostCreate.ps1
@@ -1,14 +1,23 @@
 # Setup Development Environment DevContainer
 Write-Host "Post Create Commands for Environment..."
 
+dotnet workload update
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+
 cd Api/Api.Startup.Example
 dotnet restore Startup.Api.csproj
+
 cd ../../Blazor/Blazor.Server.Startup.Example
 dotnet restore Startup.Blazor.Server.csproj
+libman restore
+
 cd ../../Console/Console.Startup.Example
 dotnet restore Startup.Console.csproj
+
 cd ../../Web/Web.Startup.Example
 dotnet restore Startup.Web.csproj
+libman restore
+
 cd ../../
 
 # Setup git Configurations
