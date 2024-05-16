c61e4b26caff1849dc1a8b3d0a11f32b7c6260c4
Michael Carey <Michael@AGameEmpowerment.com>
2024-05-16 19:10:24 +0000

    chore: Update Docker_Setup.ps1 to use relative paths for docker-compose file

--- Start_StartupExample_Api.ps1
+++ Start_StartupExample_Api.ps1
@@ -1,4 +1,16 @@
+[string] $launch_profile = $args[0]
+
+if ($null -eq $launch_profile -or $launch_profile -eq "") {
+    Write-Host "* * * *"
+    Write-Host "Valid arguments are 'http', 'https', or 'httpsalt'. Defaulting to 'https'."
+    Write-Host "* * * *"
+    $launch_profile = "https"    
+}
+
+Write-Host "Starting Application Services..."
+
 Set-Location .\src\Api\Api.Startup.Example
 dotnet build --configuration Development --source .\Api.Startup.Example.csproj
-dotnet run .\Api.Startup.Example.csproj --configuration Development --launch-profile https &
+dotnet run .\Api.Startup.Example.csproj --configuration Development --launch-profile $launch_profile &
+
 Set-Location ..\..\..\
