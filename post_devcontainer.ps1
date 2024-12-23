0d43bfb62bdb65875b4a27db0154dc9bbb441c52
Michael Carey <michael@AGameEmpowerment.com>
2024-12-22 18:11:59 -0700

    Add Docker support and update devcontainer setup
    Updated devcontainer.json:
    - Changed container name to "Dotnet Example Projects"
    - Updated base image to `mcr.microsoft.com/devcontainers/dotnet:1-9.0`
    - Added comments for better understanding
    - Included features: `azure-cli`, `node`, `nuget`
    - Added VS Code extensions: `vscode-react-native`, `bower`
    - Changed `postCreateCommand` to run `sudo pwsh -File ./post_devcontainer.ps1`
    - Updated `forwardPorts` list and attributes
    - Added `containerEnv` for environment variables
    - Set `remoteUser` to `root`
    
    Updated .gitignore to include `*.orig` files.
    
    Reordered and added new settings in settings.json.
    
    Reformatted .vsconfig without changing components.
    
    Enhanced Delete_Old_Tags.ps1 with regex pattern and output.
    
    Cleaned up docker-compose.yml by removing commented-out configs.
    
    Updated docker_setup.ps1:
    - Removed pulling of specific Docker images
    - Changed Docker compose project name to `dotnet_example`
    
    Added post_devcontainer.ps1 to set up the development environment.
    
    Added setup_project.ps1 to create/update .npmrc file.
    
    Updated launchSettings.json:
    - Renamed `httpsalt` profile to `docker_https`
    - Updated `applicationUrl` for `docker_https`
    - Removed `WSL` profile
    
    Updated Startup.Api.csproj and Startup.Aspire.ApiService.csproj:
    - Added `Docker` configuration
    - Included `appsettings.Docker.json` as a transform file
    - Added package reference for `Microsoft.VisualStudio.SlowCheetah`
    
    Added empty appsettings.Debug.json, appsettings.Docker.json, and appsettings.Release.json files.
    
    Added appsettings.Docker.json with Docker-specific settings.

---
+++ post_devcontainer.ps1
@@ -0,0 +1,23 @@
+# Setup Development Environment DevContainer
+Write-Host "Post Create Commands for Environment..."
+
+dotnet workload update
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+
+# Setup git Configurations
+git config --global credential.useHttpPath true
+git config --global core.autocrlf false
+git config --global core.eol native
+
+# Install NuGet Support
+sudo apt update
+sudo apt install -y nuget
+
+# Install .NET Core runtime
+sudo apt-get install -y dotnet-sdk-8.0
+sudo apt-get install -y dotnet-sdk-9.0
+
+# Trust HTTPS developer certificate
+dotnet dev-certs https --trust
+
+Write-Host "run  setup_project.ps1  to complete the process..."
