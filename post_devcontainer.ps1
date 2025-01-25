e5d01739e50171d0b84fa53f04b7609e73247f13
Michael Carey <Michael@AGameEmpowerment.com>
2025-01-25 20:53:54 +0000

    Add CODEOWNERS file, update VSCode settings, and configure CI/CD pipeline

--- post_devcontainer.ps1
+++ post_devcontainer.ps1
@@ -1,23 +1,27 @@
 # Setup Development Environment DevContainer
 Write-Host "Post Create Commands for Environment..."
 
+# Update the system
+sudo apt update
+sudo apt upgrade -y
+
 dotnet workload update
 dotnet tool install -g Microsoft.Web.LibraryManager.Cli
 
 # Setup git Configurations
 git config --global credential.useHttpPath true
-git config --global core.autocrlf true
-git config --global core.eol crlf
+git config --global core.autocrlf input
+#git config --global core.eol lf
 
-# Install NuGet Support
-sudo apt update
+# Install Package Manager Support
 sudo apt install -y nuget
+# sudo apt install -y npm
 
 # Install .NET Core runtime
-sudo apt-get install -y dotnet-sdk-8.0
-sudo apt-get install -y dotnet-sdk-9.0
+# sudo apt-get install -y dotnet-sdk-8.0
+# sudo apt-get install -y dotnet-sdk-9.0
 
 # Trust HTTPS developer certificate
 dotnet dev-certs https --trust
 
-Write-Host "run  setup_project.ps1  to complete the process..."
+Write-Host "run  Setup_project.ps1  to complete the process..."
