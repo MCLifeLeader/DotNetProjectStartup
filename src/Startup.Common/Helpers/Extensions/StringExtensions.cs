0996bc84696c279a6b7957beb212eab2f44db7b2
Michael Carey <Michael@AGameEmpowerment.com>
2025-03-08 11:53:54 -0700

    Add code coverage exclusions and improve documentation
    - Added `System.Diagnostics.CodeAnalysis` namespace and
      `[ExcludeFromCodeCoverage]` attribute to multiple classes
      in `DataTaxonomy.cs`, `LoggerSupport.cs`,
      `PartialSensitiveDataAttribute.cs`, `PiiDataAttribute.cs`,
      and `SensitiveDataAttribute.cs`.
    
    - Corrected namespace declaration and added summary comment
      in `DictionaryExtensions.cs`. Refactored `GetOrAdd` method
      and introduced `TryUpdate` method with detailed XML documentation.
    
    - Enhanced XML documentation for `ScrubUrlRoute` and `Mask`
      methods in `StringExtensions.cs`, and improved URL handling
      in `ScrubUrlRoute`.

--- src/Startup.Common/Helpers/Extensions/StringExtensions.cs
+++ src/Startup.Common/Helpers/Extensions/StringExtensions.cs
@@ -2,25 +2,45 @@
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Replaces double slashes in a URL with a single slash, except for 'http://' or 'https://'.
+    /// </summary>
+    /// <param name="url">The URL string to be scrubbed.</param>
+    /// <returns>The scrubbed URL string.</returns>
     public static string ScrubUrlRoute(this string url)
     {
-        if (string.IsNullOrEmpty(url))
+        if (string.IsNullOrWhiteSpace(url))
         {
             return url;
         }
 
+        if (url.StartsWith("http://") || url.StartsWith("https://"))
+        {
+            var protocol = url.StartsWith("http://") ? "http://" : "https://";
+            var restOfUrl = url.Substring(protocol.Length);
+            return protocol + restOfUrl.Replace("//", "/");
+        }
+
         return url.Replace("//", "/");
     }
+
+    /// <summary>
+    /// Masks a string by replacing characters after the first four with asterisks.
+    /// </summary>
+    /// <param name="s">The string to be masked.</param>
+    /// <returns>The masked string.</returns>
     public static string Mask(this string s)
     {
         if (string.IsNullOrWhiteSpace(s))
         {
             return s;
-        };
+        }
+
         if (s.Length <= 4)
         {
             return "****";
         }
+
         return $"{s.Substring(0, 4)}********";
     }
 }
