fa855731fa02662e6901ff8d772cbb471c5f0408
Michael Carey <Michael@AGameEmpowerment.com>
2025-06-13 12:05:37 -0600

    Agent Mode changes

--- devops/PrepNuget.ps1
+++ devops/PrepNuget.ps1
@@ -3,15 +3,12 @@ try
     # Setup the location for all of the DLLs to be copied to
     Write-Host "Creating Target Folders if they do not exist"
     New-Item -ItemType Directory -Force -Path ".\..\nugetlibs"
-    New-Item -ItemType Directory -Force -Path ".\..\nugetlibs\net8.0"
     New-Item -ItemType Directory -Force -Path ".\..\nugetlibs\net9.0"
 
     # Clean the target location
     Write-Host "Cleaning Target Folders"
     Remove-Item ".\..\nugetlibs\*.dll"
     Remove-Item ".\..\nugetlibs\*.pdb"
-    Remove-Item ".\..\nugetlibs\net8.0\*.dll"
-    Remove-Item ".\..\nugetlibs\net8.0\*.pdb"
     Remove-Item ".\..\nugetlibs\net9.0\*.dll"
     Remove-Item ".\..\nugetlibs\net9.0\*.pdb"
 
@@ -23,9 +20,6 @@ try
 
         # Write-Host "Copying $fileName"
 
-        if($fileName -match "net8.0"){
-            Copy-Item $fileName ".\..\nugetlibs\net8.0\" -Force -ErrorAction SilentlyContinue
-        }
         if($fileName -match "net9.0"){
             Copy-Item $fileName ".\..\nugetlibs\net9.0\" -Force -ErrorAction SilentlyContinue
         }
@@ -34,7 +28,6 @@ try
     # Validate that the files were copied as output to the log when run in Azure DevOps
     Write-Host "Validate Target Folder Contents"
     Get-ChildItem -Path ".\..\nugetlibs\"
-    Get-ChildItem -Path ".\..\nugetlibs\net8.0\"
     Get-ChildItem -Path ".\..\nugetlibs\net9.0\"
 }
 catch
